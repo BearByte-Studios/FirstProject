8650fb26034a80af6b3ab6617efe79b1397d1c70
LefterisDevelops <lefterisgantzos@gmail.com>
2024-12-08 22:33:07 +0200

    1st commit

---
+++ My project (1)/My project (1)/Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Character_Movement : NetworkBehaviour
+{
+    Vector3 movement;
+    float horizontalMovement;
+    float verticalMovement;
+
+    float speed = 50f;
+
+    float movement_num;
+
+    public Rigidbody rb;
+
+    
+    
+     
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsOwner)
+        {
+          Controller();
+        }
+    }
+
+
+    public void Controller()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        movement_num = Mathf.Abs(verticalMovement) + Mathf.Abs(horizontalMovement);
+
+        movement = new Vector3(horizontalMovement, 0, verticalMovement).normalized;
+        if (movement_num > 0)
+        {
+           rb.AddForce(movement * speed * 10f * Time.deltaTime );
+           transform.rotation = Quaternion.LookRotation(movement);
+           rb.linearDamping = .2f;
+
+        }
+
+        
+
+       
+
+    }
+}
